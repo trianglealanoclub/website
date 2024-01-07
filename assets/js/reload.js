a45c221194a1aeae684abd6c1c5d8999663f2c60
Brad Bunce <76120543+bradbunce@users.noreply.github.com>
2024-01-07 08:37:12 -0500

    update index.html

--- assets/js/reload.js
+++ assets/js/reload.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Match this timestamp with the release of your code
-    var lastVersioning = Date.UTC(2024, 0, 7, 6, 31);
+    var lastVersioning = Date.UTC(2024, 0, 7, 8, 36);
 
     var lastCacheDateTime = localStorage.getItem('lastCacheDateTime');
 
